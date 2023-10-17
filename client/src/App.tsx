83d705e47fdbad1855001168d68ba69be65850c6
OlAndrey <oleynik.andrey01@gmail.com>
2023-10-17 19:49:45 +0300

    chore(client): added the ability to create an online game

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 /* eslint-disable react-hooks/exhaustive-deps */
 import { MutableRefObject, useRef, useState } from 'react'
 import Modal from './Components/Modal'
@@ -12,6 +13,52 @@ function App() {
       <button className="btn" onClick={() => console.log('clicked!')}>
         Connect
       </button>
+=======
+import { useState } from 'react'
+import Modal from './Components/Modal'
+import socket from './helpers/socket'
+
+function App() {
+  const [showModal] = useState(true)
+  const [isNewGame, setIsNewGame] = useState(false)
+  const data = [
+    { time: '5', name: 'Blitz' },
+    { time: '10', name: 'Rapid' },
+    { time: '30', name: 'Classical' },
+    { time: '-', name: 'Unlimit' }
+  ]
+
+  const createGame = (option: (typeof data)[0]) => {
+    socket.emit('start', { time: option.time })
+    console.log(option)
+  }
+
+  return (
+    <Modal className={showModal ? 'bg-image' : ''} isShow={showModal}>
+      {!isNewGame ? (
+        <button className="btn" onClick={() => setIsNewGame(true)}>
+          Create
+        </button>
+      ) : (
+        <>
+          <div className="flex">
+            <span className="text-lg">Please select game type!</span>
+            <button className="btn-close" onClick={() => setIsNewGame(false)}>
+              &times;
+            </button>
+          </div>
+
+          <div className="lpoll">
+            {data.map((item) => (
+              <div key={Math.random()} onClick={() => createGame(item)}>
+                <div className="clock">{item.time} min</div>
+                <div className="perf">{item.name}</div>
+              </div>
+            ))}
+          </div>
+        </>
+      )}
+>>>>>>> cd65d85 (chore(client): added the ability to create an online game)
     </Modal>
   )
 }
