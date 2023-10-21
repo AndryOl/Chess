ce95253ed22d37025244e2c6ebb9bea628daa5d7
OlAndrey <oleynik.andrey01@gmail.com>
2023-10-21 21:12:04 +0300

    refactor(client): connection to the game has been moved to a separate component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,60 +1,16 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-/* eslint-disable react-hooks/exhaustive-deps */
-import { MutableRefObject, useRef, useState } from 'react'
-import Modal from './Components/Modal'
-
-function App() {
-  const [showModal] = useState(true)
-  const inputRef = useRef() as MutableRefObject<HTMLInputElement>
-
-  return (
-    <Modal className={showModal ? 'bg-image' : ''} isShow={showModal}>
-      <input type="text" ref={inputRef} placeholder="Enter game id" />
-      <button className="btn" onClick={() => console.log('clicked!')}>
-        Connect
-      </button>
-=======
-import { useState } from 'react'
-=======
-import { MutableRefObject, useRef, useState } from 'react'
->>>>>>> 39bbec5 (chore(client): added processing of connecting to an online game)
-import Modal from './Components/Modal'
-import socket from './helpers/socket'
-import CreateGame from './Components/CreateGame'
+import { Routes, Route, Navigate } from 'react-router-dom'
+import Game from './Components/Game'
+import PlayerWaiting from './Components/PlayerWaiting'
+import ConnectGame from './Components/ConnectGame'
 
 const App = () => {
-  const [showModal] = useState(true)
-  const [isNewGame, setIsNewGame] = useState(false)
-  const inputRef = useRef() as MutableRefObject<HTMLInputElement>
-
-  const connect = () => {
-    if (inputRef.current.value.trim()) {
-      socket.emit('check-token', { token: inputRef.current.value })
-      socket.on('full', () => console.error('room is full'))
-      socket.on('token-invalid', () => console.error('invalid'))
-      socket.on('token-ok', () => console.log('token is ok'))
-    }
-  }
-
   return (
-    <Modal className={showModal ? 'bg-image' : ''} isShow={showModal}>
-      {!isNewGame ? (
-        <>
-          <input type="text" ref={inputRef} placeholder="Enter game id" />
-          <button className="btn bg-black" onClick={connect}>
-            Connect
-          </button>
-          <div className="text-center">or</div>
-          <button className="btn bg-green" onClick={() => setIsNewGame(true)}>
-            Create
-          </button>
-        </>
-      ) : (
-        <CreateGame handler={setIsNewGame} />
-      )}
->>>>>>> cd65d85 (chore(client): added the ability to create an online game)
-    </Modal>
+    <Routes>
+      <Route path="/game/:token" Component={Game} />
+      <Route path="/:token" Component={PlayerWaiting} />
+      <Route path="/" Component={ConnectGame} />
+      <Route path="/*" element={<Navigate to="/" replace={true} />} />
+    </Routes>
   )
 }
 
